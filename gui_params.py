ec8b242007d2e7acb9fdb42538d9108944d97268
dasalam <dasalam@gmail.com>
2018-03-20 17:08:23 +0300

    Separate color names for mpl det

--- gui_params.py
+++ gui_params.py
@@ -4,6 +4,7 @@ t = ["D1-D2", "D2-D1", "D1-D3", "D3-D1",
      "D2-D4", "D4-D2", "D3-D4", "D4-D3"]
 
 det_colors = ["red", "green", "blue", "magenta"]
+det_mpl_colors = ["r", "g", "b", "m"]
 t_spk_colors = ["#ff0000", "#ff6565", "#008000", "#3cbc3c",
                 "#0000ff", "#6565ff", "#bf00bf", "#cd6fcc",
                 "#000000", "#656565", "#00bfbf", "#63ddde"]
--- histopac.py
+++ histopac.py
@@ -1065,7 +1065,7 @@ class Create_UI(Gtk.Window):
         
         for i in range(det_num):
             self.en_lines.append( self.ax_en.plot(x, self.en_spk[i], marker="o",
-                                                  ms=3, mew=0, c=gui_params.det_colors[i], lw=1.0)[0] )
+                                                  ms=3, mew=0, c=gui_params.det_mpl_colors[i], lw=1.0)[0] )
         self.set_lim_vals_en(0)     
         self.canvas_en.draw()
 
